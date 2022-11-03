2dadc3e169d1cc3aa03c9e0e647a858e93b52d87
mturchik <mturchik@batteriesplus.com>
2022-11-02 23:28:37 -0500

    Use non-top-level program

--- BlizzardApiProxy/Program.cs
+++ BlizzardApiProxy/Program.cs
@@ -1,26 +1,35 @@
 using BlizzardApiProxy.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddHttpClient();
-builder.Services.AddSingleton<TokenService>();
-builder.Services.AddScoped<ProfileService>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace BlizzardApiProxy;
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddHttpClient();
+        builder.Services.AddSingleton<TokenService>();
+        builder.Services.AddScoped<ProfileService>();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
